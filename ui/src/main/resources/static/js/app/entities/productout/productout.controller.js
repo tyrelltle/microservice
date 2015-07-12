aede998a5fc5f700b9b96991db3d456961d4f839
tyrelltle <tyrelltle@gmail.com>
2015-07-12 13:09:21 -0700

    print product checkout list

--- ui/src/main/resources/static/js/app/entities/productout/productout.controller.js
+++ ui/src/main/resources/static/js/app/entities/productout/productout.controller.js
@@ -2,40 +2,76 @@
 
 angular.module('jhipsterApp')
     .controller('ProductOutController', function ($scope, ProductCheckoutServices, ParseLinks, $modal) {
-        $scope.checkout_state={
-            sku:-1
-        };
-        $scope.products = [];
-        $scope.page = 1;
+        $scope.modes={
+            new:{
+                title:"Scan product to checkout",
+                products:[]
+            },
+            hist:{
+                title:"Historical Checked Out Products",
+                products:[]
+            }
+        }
 
-        $scope.loadAll = function() {
+        $scope.radioModel="hist";
+
+        $scope.currentmode=$scope.modes.hist;
+
+        $scope.switchToNewMode=function(){
+            $scope.currentmode=$scope.modes.new;
+            $scope.radioModel="new";
+        }
+        $scope.switchToHistMode=function(){
+            $scope.currentmode=$scope.modes.hist;
+            $scope.radioModel="hist";
+            $scope.loadHistCheckouts();
+        }
+
+        $scope.clearNewProducts=function(){
+            $scope.modes.new.products=[];
+        }
+
+        $scope.$watch('radioModel', function(newValues, oldValues, scope) {
+           if(newValues=="new") {
+               $scope.switchToNewMode();
+           }else if(newValues=="hist"){
+               $scope.switchToHistMode();
+           }else{
+               alert("weird radio model state!");
+           }
+         });
+
+        $scope.loadHistCheckouts = function() {
             ProductCheckoutServices.query(function(result,link) {
                 $scope.links = ParseLinks.parse(link);
                 for (var i = 0; i < result.length; i++) {
-                    $scope.products.push(result[i]);
+                    $scope.modes.hist.products.push(result[i]);
                 }
             });
         };
+        $scope.switchToHistMode();
+        $scope.checkout_state={
+            sku:-1
+        };
+        $scope.page = 1;
+
+
         $scope.reset = function() {
             $scope.page = 1;
-            $scope.products = [];
-            $scope.loadAll();
+            $scope.modes.new.products = [];
+            $scope.modes.hist.products = [];
+            //$scope.loadHistCheckouts();
         };
         $scope.loadPage = function(page) {
             $scope.page = page;
-            $scope.loadAll();
+            //$scope.loadHistCheckouts();
         };
 
-        $scope.showUpdate = function (id) {
-            Product.get({id: id}, function(result) {
-                $scope.product = result;
-                $('#saveProductModal').modal('show');
-            });
-        };
 
 
 
         $scope.checkout = function (how) {
+            $scope.currentmode=$scope.modes.new;
 
             if(how!='sku' && how!='qr')
                 throw new Error('what the ...');
@@ -53,9 +89,9 @@ angular.module('jhipsterApp')
 
 
             modalInstance.result.then(function (sku) {
-                ProductCheckoutServices.checkout(sku, function () {
+                ProductCheckoutServices.checkout(sku, function (data) {
                     alert('success!');
-                    $scope.refresh();
+                    $scope.modes.new.products.unshift(data);
                 });
             });
 
@@ -76,6 +112,22 @@ angular.module('jhipsterApp')
            // $scope.editForm.$setUntouched();
         };
 
+        $scope.print=function(){
+            var mywindow = window.open('', 'my div', 'height=400,width=600');
+            mywindow.document.write('<html><head><title>my div</title>');
+            /*optional stylesheet*/ //mywindow.document.write('<link rel="stylesheet" href="main.css" type="text/css" />');
+            mywindow.document.write('</head><body >');
+            mywindow.document.write($('.prodtab').html());
+            mywindow.document.write('</body></html>');
+
+            mywindow.document.close(); // necessary for IE >= 10
+            mywindow.focus(); // necessary for IE >= 10
+
+            mywindow.print();
+            mywindow.close();
+
+            return true;
+        }
 
 
      });
