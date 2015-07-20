49d70d16af63c1dfdfe549471b68f90a0e6ea381
EC2 Default User <ec2-user@ip-172-31-27-198.us-west-2.compute.internal>
2015-07-20 03:11:47 +0000

    fixing script error

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd microservices/api/product-api-service; sudo ./gradlew distDocker; cd-
-cd microservices/support/auth-server; sudo ./gradlew distDocker; cd-
-cd microservices/support/discovery-server; sudo ./gradlew distDocker; cd-
-cd microservices/support/edge-server; sudo ./gradlew distDocker; cd-
+cd microservices/api/product-api-service; sudo ./gradlew distDocker; cd -
+cd microservices/support/auth-server; sudo ./gradlew distDocker; cd -
+cd microservices/support/discovery-server; sudo ./gradlew distDocker; cd -
+cd microservices/support/edge-server; sudo ./gradlew distDocker; cd -
